f9af59a5ce6b5bb7b757aee763c79b4ae3eebda4
Vedangi Deshpande <vedangi.deshpande18@vit.edu>
2022-03-16 12:05:30 +0530

    Chnges for connection of db

--- login.js
+++ login.js
@@ -37,34 +37,34 @@ app.get('/dashboard', function(request, response) {
 	response.sendFile(path.join(__dirname + '/dashboard.html'));
 });
 
-// // http://localhost:3000/auth
-// app.post('/auth', function(request, response) {
-// 	// Capture the input fields
-// 	let userid = request.body.userid;
-// 	let password = request.body.password;
-// 	// Ensure the input fields exists and are not empty
-// 	if (userid && password) {
-// 		// Execute SQL query that'll select the account from the database based on the specified username and password
-// 		connection.query('SELECT * FROM users WHERE userid = ? AND password = ?', [userid, password], function(error, results, fields) {
-// 			// If there is an issue with the query, output the error
-// 			if (error) throw error;
-// 			// If the account exists
-// 			if (results.length > 0) {
-// 				// Authenticate the user
-// 				request.session.loggedin = true;
-// 				request.session.userid= userid;
-// 				// Redirect to home page
-// 				response.redirect('/dashboard');
-// 			} else {
-// 				response.send('Incorrect Username and/or Password!');
-// 			}			
-// 			response.end();
-// 		});
-// 	} else {
-// 		response.send('Please enter Username and Password!');
-// 		response.end();
-// 	}
-// });
+// http://localhost:3000/auth
+app.post('/auth', function(request, response) {
+	// Capture the input fields
+	let userid = request.body.userid;
+	let password = request.body.password;
+	// Ensure the input fields exists and are not empty
+	if (userid && password) {
+		// Execute SQL query that'll select the account from the database based on the specified username and password
+		con.query('SELECT * FROM users WHERE userid = ? AND password = ?', [userid, password], function(error, results, fields) {
+			// If there is an issue with the query, output the error
+			if (error) throw error;
+			// If the account exists
+			if (results.length > 0) {
+				// Authenticate the user
+				request.session.loggedin = true;
+				request.session.userid= userid;
+				// Redirect to home page
+				response.redirect('/dashboard');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
 
 // // http://localhost:3000/home
 // app.get('/dashboard', function(request, response) {
