00956f2d668c11399cb56b074aa504d6dc9c22ca
Vedangi Deshpande <vedangi.deshpande18@vit.edu>
2022-03-23 00:52:21 +0530

    Added emp registeraton page but having some issue in registreation

--- login.js
+++ login.js
@@ -9,7 +9,7 @@ var reportlist = ["PF Format", "ESIC Format", "NEFT Format", "Custom Report", "l
 var settinglist = ["Site Settings", "Employee Settings", "Change Password", "Salary Settings", "Log Out"];
 const con = mysql.createConnection({
 	host     : 'us-cdbr-east-05.cleardb.net',
-    port : 3306,
+    port : 3307,
 	user     : 'b0c522ab6e71be',
 	password : '079c45a6',
 	database : 'heroku_e064bc22a0a0a92'
@@ -130,17 +130,41 @@ app.post('/auth', function(request, response) {
 
 		console.log('req.body');
 		console.log(req.body);
-		res.write('You sent the EMP_ID "' + req.body.EMP_ID+'".\n');
+
+		// page 1
+		res.write('You sent the Firm "' + req.body.FIRM+'".\n');
 		res.write('You sent the SITE_ID "' + req.body.SITE_ID+'".\n');
 		res.write('You sent the EMP_NAME "' + req.body.EMP_NAME+'".\n');
+		res.write('You sent the EMP_MOBILE"' + req.body.EMP_MOBILE+'".\n');
+		res.write('You sent the AADHAR "' + req.body.AADHAR+'".\n');
+		res.write('You sent the EMP_ADDRESS "' + req.body.EMP_ADDRESS+'".\n');
+		res.write('You sent the PAN "' + req.body.PAN+'".\n');
 		
-		res.end()
+// page 2
+		res.write('You sent the UAN_NO "' + req.body.UAN_NO+'".\n');
+		res.write('You sent the ESIC_NO "' + req.body.ESIC_NO+'".\n');
+		res.write('You sent the BANK_AC_NO "' + req.body.BANK_AC_NO+'".\n');
+		res.write('You sent the IFSC_CODE "' + req.body.IFSC_CODE+'".\n');
+		res.write('You sent the BNK_NAME "' + req.body.BNK_NAME+'".\n');
+		res.write('You sent the BNK_ADDRESS "' + req.body.BNK_ADDRESS+'".\n');
 		
-		con.query("Insert into emp_details(EMP_ID, SITE_ID,EMP_NAME) VALUES ('"+req.body.EMP_ID+"' , '"+req.body.SITE_ID+"','"+req.body.EMP_NAME+"')",function(err, result)      
+		// page 3
+		res.write('You sent the DESIGNATION "' + req.body.DESIGNATION+'".\n');
+		res.write('You sent the CATEGORY "' + req.body.CATEGORY+'".\n');
+		res.write('You sent the BASIC_SAL "' + req.body.BASIC_SAL+'".\n');
+		res.write('You sent the HRA_SAL "' + req.body.HRA_SAL+'".\n');
+		res.write('You sent the SPL_ALLOW "' + req.body.SPL_ALLOW+'".\n');
+		res.write('You sent the STATUS "' + req.body.STATUS+'".\n');
+		
+		con.query("Insert into emp_details(FIRM, SITE_ID,EMP_NAME,EMP_MOBILE,AADHAR,EMP_ADDRESS,PAN,UAN_NO,ESIC_NO,BANK_AC_NO,IFSC_CODE,BNK_NAME,BNK_ADDRESS,DESIGNATION,CATEGORY,BASIC_SAL,HRA_SAL,SPL_ALLOW,STATUS) VALUES('"+req.body.FIRM+"' , '"+req.body.SITE_ID+"','"+req.body.EMP_NAME+"' , '"+req.body.EMP_MOBILE+"' , '"+req.body.AADHAR+"' , '"+req.body.EMP_ADDRESS+"', '"+req.body.PAN+"','"+req.body.UAN_NO+"', '"+req.body.ESIC_NO+"', '"+req.body.BANK_AC_NO+"', '"+req.body.IFSC_CODE+"', '"+req.body.BNK_NAME+"', '"+req.body.BNK_ADDRESS+"','"+req.body.DESIGNATION+"', '"+req.body.CATEGORY+"', '"+req.body.BASIC_SAL+"', '"+req.body.HRA_SAL+"', '"+req.body.SPL_ALLOW+"', '"+req.body.STATUS+"')",function(err, result)     
 		{                                                      
 		  if (err)
 			 throw err;
+		colsole.log("success");
 		});
+		res.end()
+		
+		
 		});
 
 //register logic end
